4128bbd7f7bd600a3731ee35519ed77f06d4fe98
gtmle <losgeorgios@gmail.com>
2024-01-28 06:05:15 +0200

    exercises

---
+++ homework3.3.py
@@ -0,0 +1,7 @@
+answer = 22
+question = "How much does  15 +7? "
+
+while int(input(question)) != answer:
+    pass
+
+print("Correct")
--- readme.txt
+++ readme.txt
@@ -1,2 +1,4 @@
 Welcome to my Homework
+Made for Skillo Python course
+by Tsvetan Tsvetanov
 Author Georgios Los
